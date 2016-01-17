84662b17358c25e262c82ccf5e495d5abf83b2d5
郭亭巖 <kuoldstone@gmail.com>
2016-01-17 13:54:39 +0800

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  namespace :account do
+    resources :groups
+    resources :posts
+  end
+
+
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +61,14 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+  root 'groups#index'
+  resources:groups do
+
+    member do
+      post :join
+      post :quit
+    end
+
+    resources :posts
+  end
+  end
